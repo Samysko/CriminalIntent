72d133e463b9044a56abaa3d4315b605c5393ead
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-05-28 05:59:15 -0500

    Setup the ViewPager to show Crimes List
    Now the ViewPager will show the list of crimes inside fragments.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
@@ -2,12 +2,39 @@ package com.bignerdranch.criminalintent;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 public class CrimePagerActivity extends AppCompatActivity {
+    ViewPager mViewPager;
+    List<Crime> mCrimes;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        mCrimes = CrimeLab.get(this).getCrimes();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int i) {
+                Crime crime = mCrimes.get(i);
+
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
