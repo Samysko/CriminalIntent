c52db1b819b17dfbbbb90c1fcebb361d8861eca0
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-05-28 06:15:45 -0500

    Update CrimeListFragment to initialize CrimePagerActivity
    Now CrimeListFragment is going to start Pageractivity when an item inside of it is selected by the user.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,19 +11,30 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
     ViewPager mViewPager;
     List<Crime> mCrimes;
+
+    public static final String EXTRA_CRIME_ID = "com.bignerdranch.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
 
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
         mViewPager = findViewById(R.id.crime_view_pager);
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         mCrimes = CrimeLab.get(this).getCrimes();
         mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
             @Override
