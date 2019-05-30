fe93e89caf12f1add0df7072454cb78a2427db6b
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-05-30 06:14:18 -0500

    Fix CrimePagerActivity's PagerAdapter
    The correct PagerAdapter to use is FragmentStatePagerAdapter not FragmentPagerAdapter.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimePagerActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -36,7 +36,7 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         mCrimes = CrimeLab.get(this).getCrimes();
-        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int i) {
                 Crime crime = mCrimes.get(i);
