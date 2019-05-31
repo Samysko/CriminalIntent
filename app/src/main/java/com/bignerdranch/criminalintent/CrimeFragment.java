c2b23b6de33fa3cbc4493c7a57a03e9df0bed0f9
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-05-31 06:21:25 -0500

    Update CrimeFragment to show DatePickerFragment
    CrimeFragment is going to show DatePickerFragment setting its Date Button with a onClickListener to show the Dialog.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -22,7 +23,8 @@ public class CrimeFragment extends Fragment {
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
-    private static final String ARG_CRIME_ID = "com.bignerdranch.criminalinten.crime_id";
+    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     public static CrimeFragment newInstance(UUID uuid){
         Bundle args = new Bundle();
@@ -68,7 +70,15 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = view.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+                
+            }
+        });
 
         mSolvedCheckBox = view.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
