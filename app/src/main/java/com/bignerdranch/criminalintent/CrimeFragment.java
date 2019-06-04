8f62eb08d28c147bd964ff37fcdc66a9477544c8
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-04 06:24:29 -0500

    Update CrimeFragment overriding onActivityResult
    When the linked Fragment (DatePickerFragment) is destroyed, data is sended to its Target Fragment as a Result, now that result is going to be used to update the Date Button.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -94,5 +95,14 @@ public class CrimeFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Date date = (Date) data.getExtras().getSerializable(DatePickerFragment.EXTRA_DATE);
+        mCrime.setDate(date);
+        mDateButton.setText(mCrime.getDate().toString());
     }
 }
