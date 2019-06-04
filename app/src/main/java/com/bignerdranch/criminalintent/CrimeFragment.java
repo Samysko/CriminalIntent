1b28cd3dd93fee6ffb3a57d6edc45342713ac262
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-03 21:15:54 -0500

    Update DatePickerFragment to receive a date
    The DatePickerFragment know is able to receive the Crime's Date, turn it into a Calendar object and put the Year, Month and Day inside the DatePicker after inflate the layout.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -15,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
@@ -74,7 +75,7 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(manager, DIALOG_DATE);
 
             }
--- app/src/main/java/com/bignerdranch/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/DatePickerFragment.java
@@ -8,13 +8,42 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+
+    DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.setArguments(bundle);
+        return datePickerFragment;
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        mDatePicker = v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
