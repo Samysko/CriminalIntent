ea3f3031cc65d386bdebe053e399001c56d7669b
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-09 22:04:03 -0500

    Update DatePickerFragment to use onCreateView
    Instead of use Dialog's onCreateDialog method to return a Dialog now DatePickerFragment is going to return a View.

--- app/src/main/java/com/bignerdranch/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/DatePickerFragment.java
@@ -11,6 +11,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
@@ -24,6 +26,7 @@ public class DatePickerFragment extends DialogFragment {
     public static final String EXTRA_DATE = "com.bignerdranch.criminalintent.date";
 
     DatePicker mDatePicker;
+    Button mButton;
 
     public static DatePickerFragment newInstance(Date date){
         Bundle bundle = new Bundle();
@@ -34,7 +37,43 @@ public class DatePickerFragment extends DialogFragment {
         return datePickerFragment;
     }
 
-    @NonNull
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_date, container);
+
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        mDatePicker = view.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
+
+        mButton = view.findViewById(R.id.ok_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+
+                Date date = new GregorianCalendar(year, month, day).getTime();
+                sendResult(Activity.RESULT_OK, date);
+
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .remove(DatePickerFragment.this).commit();
+            }
+        });
+
+        return view;
+    }
+
+    /*@NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Date date = (Date) getArguments().getSerializable(ARG_DATE);
@@ -66,7 +105,7 @@ public class DatePickerFragment extends DialogFragment {
                     }
                 })
                 .create();
-    }
+    }*/
 
     private void sendResult(int resultCode, Date date){
         Intent intent = new Intent();
