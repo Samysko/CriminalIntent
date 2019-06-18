9e5ebb659aae527f19ba70bc173331662f4aefa7
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-17 21:05:15 -0500

    Update CrimeListFragment to use the new Toolbar's item
    Since the Layout of the Menu is created it can be inflated into the toolbar by CrimeListFragment's callback method named onCreateOptionsMenu and added by it's another method named setHasOptionsMenu.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -22,10 +24,23 @@ public class CrimeListFragment extends Fragment {
     private TextView mDateTextView;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
 
         updateUI();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_cirme_list, menu);
     }
 
     @Nullable
