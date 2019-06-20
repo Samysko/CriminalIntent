e657685c302a41edfa03eba1e9dd9676e4d47b83
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-20 06:42:02 -0500

    Update strings.xml to add plurals
    To be grammatically correct the Toolbar's subtitle needs to say whether is one crime or many crimes. so in this case "crime" and in plural "crimes" have been added.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -2,7 +2,6 @@ package com.bignerdranch.criminalintent;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.MenuRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
