6ba667ca1d5942184c810fd62b9555285904df47
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-21 06:42:23 -0500

    Add columns to Crimes table
    An inner class named Cols is going to contain the Table's columns

--- app/src/main/java/com/bignerdranch/criminalintent/database/CrimeDbScherma.java
+++ app/src/main/java/com/bignerdranch/criminalintent/database/CrimeDbScherma.java
@@ -4,6 +4,11 @@ public class CrimeDbScherma {
     public static final class CrimeTable{
         public static final String NAME = "crimes";
 
-        
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
     }
 }
