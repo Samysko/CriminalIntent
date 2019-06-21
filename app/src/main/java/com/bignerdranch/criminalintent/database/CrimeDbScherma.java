cf60994b8ce17645410c837a14676a8c8578ef39
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-21 06:38:20 -0500

    Update CrimeDbSchema with Crimes table
    In order to crete a new Table inside CrimeDbSchema a constant inner static class is needed with the constant name of the table.

--- app/src/main/java/com/bignerdranch/criminalintent/database/CrimeDbScherma.java
+++ app/src/main/java/com/bignerdranch/criminalintent/database/CrimeDbScherma.java
@@ -1,4 +1,9 @@
 package com.bignerdranch.criminalintent.database;
 
 public class CrimeDbScherma {
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+
+        
+    }
 }
