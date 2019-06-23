516f8873920df044fc6ae30603ff7c320909d3c4
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-22 19:33:06 -0500

    Create CrimeBaseHelper
    This class is going to help to open or update the DB inside the app.

---
+++ app/src/main/java/com/bignerdranch/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    public static final int VERSION = 1;
+    public static final String DATABASE_NAME = "crimeBase.db";
+
+    /**
+     *
+     * @param context
+     */
+    public CrimeBaseHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
