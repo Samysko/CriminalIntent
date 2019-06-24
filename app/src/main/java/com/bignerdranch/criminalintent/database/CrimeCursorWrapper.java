0183b02fcefe4faa50ec7cb7a337e2ec5e33f3cb
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-06-24 07:03:48 -0500

    Create CrimeCursorWrapper
    CrimeCursorWrapper's intention is to wrap a Cursor that will help avoiding rewrite code to obtain Crimes each time that we read from the DB.

---
+++ app/src/main/java/com/bignerdranch/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+}
