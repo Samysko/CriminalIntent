909bccce0d14910bbfcd22184d19c61295c187d1
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-07-24 06:32:28 -0500

    Update CrimeListActivity to start CrimeFragment
    Depending of which kind of layout android has selected to use, CrimeListActivity will create a new CrimePagerActivity or add one CrimeFragment to the layout.

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeListActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeListActivity extends SingleFragmentActivity
         implements CrimeListFragment.Callbacks{
@@ -11,7 +13,15 @@ public class CrimeListActivity extends SingleFragmentActivity
 
     @Override
     public void onCrimeSelected(Crime c) {
-        
+        if(findViewById(R.id.detail_fragment_container) == null){
+            Intent intent = CrimePagerActivity.newIntent(this, c.getId());
+            startActivity(intent);
+        }else{
+            Fragment fragment = CrimeFragment.newInstance(c.getId());
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, fragment)
+                    .commit();
+        }
     }
 
     @Override
