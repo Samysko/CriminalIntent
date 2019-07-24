a9545b67b17e0c4ad8feac3a8ba1089052c58edc
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-07-24 06:14:07 -0500

    Update CrimeListActivity to implement CrimeListFragment.Callbacks

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeListActivity.java
@@ -2,10 +2,16 @@ package com.bignerdranch.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeListActivity extends SingleFragmentActivity{
+public class CrimeListActivity extends SingleFragmentActivity
+        implements CrimeListFragment.Callbacks{
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    public void onCrimeSelected(Crime c) {
+        
     }
 
     @Override
